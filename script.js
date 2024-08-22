5d1016f3f94e4dac944d0c3807878232e60afe35
wang potao <zhuyuqing22@gmail.com>
2024-08-22 13:41:13 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(function () {
+        alert("Trivia Time! Did you know this fan page subject won an award in 2020?");
+    }, 3000);  // Show after 3 seconds
+});
 

6e25143dd69ce6445eecaf1c31659fe976bb1996
wang potao <zhuyuqing22@gmail.com>
2024-08-22 13:43:46 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
 document.addEventListener("DOMContentLoaded", function () {
-    setTimeout(function () {
-        alert("Trivia Time! Did you know this fan page subject won an award in 2020?");
-    }, 3000);  // Show after 3 seconds
+    console.log("Justin Bieber Fan Page loaded!");
 });
-
